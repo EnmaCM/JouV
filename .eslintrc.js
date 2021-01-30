f1b91af49afebb53e6c28b0ff730fdb0f152568b
Ronel Enmanuel <enmanuelcruz252@gmail.com>
2021-01-30 16:16:36 -0400

    Style details

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,32 @@
+// {
+//   "env": {
+//       "browser": true,
+//       "commonjs": true,
+//       "es2021": true,
+//       "node": true,
+//       "es6": true
+//   },
+//   "extends": "eslint:recommended",
+  
+//   "parserOptions": {
+//       "ecmaVersion": 12,
+//       "ecmaFeatures": {
+//         "impliedStrict": true
+//       }
+//   },
+//   "rules": {
+//   }
+// }
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "html": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12
-    },
-    "rules": {
-    }
+  "env": {
+      "browser": true,
+      "es2021": true,
+      "html": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+      "ecmaVersion": 12
+  },
+  "rules": {
+  }
 };
